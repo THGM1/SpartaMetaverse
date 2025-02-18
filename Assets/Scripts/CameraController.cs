d8f79ebf720afdc26cf59743b96ae168db8f2206
THGM1 <jennypsh@naver.com>
2025-02-18 17:57:52 +0900

    [FEAT] 카메라 맵 이동 제한

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,6 +8,9 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float cameraSpeed = .1f;
     [SerializeField] float playerXPos;
     [SerializeField] float playerYPos;
+    [SerializeField] Vector2 minPos;
+    [SerializeField] Vector2 maxPos;
+
     Camera camera;
 
     private void Awake()
@@ -38,9 +41,12 @@ public class CameraController : MonoBehaviour
         GameObject player = GameObject.FindGameObjectWithTag("Player");
         Vector2 playerPos = player.transform.position;
 
+        //플레이어 제한
         position.x = Mathf.Clamp(position.x, playerPos.x - playerXPos, playerPos.x + playerXPos);
         position.y = Mathf.Clamp(position.y, playerPos.y - playerYPos, playerPos.y + playerYPos);
-
+        //맵 제한
+        position.x = Mathf.Clamp(position.x, minPos.x, maxPos.x);
+        position.y = Mathf.Clamp(position.y, minPos.y, maxPos.y);
         transform.position = position;
     }
 }
