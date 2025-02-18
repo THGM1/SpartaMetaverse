26ea272f3deef1467cf9b0495505db49a158ed35
THGM1 <jennypsh@naver.com>
2025-02-18 15:44:39 +0900

    [FEAT] 상호작용 오브젝트 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    [SerializeField] private Transform carryPosition;
+    private GameObject carriedObject;
+    private GameObject nearbyObject;
     [SerializeField] private float moveSpeed = .1f;
     private Animator animator;
     private bool isMoving;
+    private Vector2 lastDirection = Vector2.down;
 
     private void Start()
     {
@@ -18,6 +21,7 @@ public class Player : MonoBehaviour
     private void Update()
     {
         Move();
+        HandleObjectPickup();
     }
     private void Move()
     {
@@ -33,5 +37,61 @@ public class Player : MonoBehaviour
 
         transform.position += new Vector3(horizontal, vertical) * Time.deltaTime;
     }
-    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovableObject") && carriedObject == null)
+        {
+            nearbyObject = collision.gameObject;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("MovableObject"))
+        {
+            nearbyObject = null;
+        }
+    }
+
+    private void PickUp()
+    {
+        if (nearbyObject != null)
+        {
+            carriedObject = nearbyObject;
+            carriedObject.transform.position = carryPosition.position;
+            carriedObject.transform.SetParent(carryPosition);
+            carriedObject.GetComponent<Rigidbody2D>().isKinematic = true;
+            nearbyObject = null;
+        }
+    }
+
+    private void DropObject()
+    {
+        carriedObject.transform.SetParent(null);
+        carriedObject.GetComponent<Rigidbody2D>().isKinematic=false;
+
+        Vector2 dropPosition = (Vector2)transform.position + lastDirection;
+
+        if (!Physics2D.OverlapCircle(dropPosition, 0.5f, LayerMask.GetMask("Wall")))
+        {
+            carriedObject.transform.position = dropPosition;
+        }
+        else
+        {
+            carriedObject.transform.position = transform.position;
+        }
+        carriedObject = null;
+    }
+
+    private void HandleObjectPickup()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (carriedObject == null)
+            {
+                PickUp();
+            }
+            else DropObject();
+        }
+    }
+
 }
