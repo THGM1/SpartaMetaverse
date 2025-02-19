e54806c64a9fc31ca04ab3850621a790b450a19d
THGM1 <jennypsh@naver.com>
2025-02-19 11:53:34 +0900

    [FEAT] 장애물 랜덤 생성

---
+++ Assets/Scripts/MinigamePlayer.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigamePlayer : MonoBehaviour
+{
+    Animator animator = null;
+    Rigidbody2D rb = null;
+
+    public float jumpForce = 5f;
+    public float forwardSpeed = 3f;
+    public bool isDead = false;
+    float deathCooldown = 0f;
+    bool isJump = false;
+
+    private void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        animator.SetBool("IsDie", false);
+    }
+    private void Update()
+    {
+        if (isDead)
+        {
+            if (deathCooldown < 0f)
+            {
+                if (Input.anyKeyDown)
+                {
+                    //재시작
+                }
+            }
+            else deathCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("점프");
+                isJump = true;
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (isDead) return;
+
+        Vector3 velocity = rb.velocity;
+        velocity.x = forwardSpeed;
+        if (isJump)
+        {
+            velocity.y += jumpForce;
+            isJump = false;
+        }
+
+        rb.velocity = velocity;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            animator.SetBool("IsDie", true);
+            isDead = true;
+            deathCooldown = 1f;
+        }
+    }
+}
---
+++ Assets/Scripts/ObstacleManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] obstacles; // 장애물 프리팹
+    [SerializeField] private float spawnInterval = 2f; // 생성 간격
+    [SerializeField] private float widthPadding; // 장애물 간격
+    [SerializeField] private Transform spawnPoint; // 생성 위치
+    private Vector3 lastPosition; // 마지막으로 생성된 장애물 위치
+
+    private void Start()
+    {
+        lastPosition = spawnPoint.position;
+        StartCoroutine(SpawnObstacles());
+    }
+
+    private IEnumerator SpawnObstacles()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnInterval);
+            SetRandomPosition();
+        }
+    }
+    public void SetRandomPosition()
+    {
+        if (obstacles.Length == 0) return;
+        widthPadding =Random.Range(3f, 8f);
+        int randomIndex = Random.Range(0, obstacles.Length);
+        GameObject obstacle = Instantiate(obstacles[randomIndex]);
+        lastPosition += new Vector3(widthPadding, 0, 0);
+        obstacle.transform.position = lastPosition;
+
+    }
+}
